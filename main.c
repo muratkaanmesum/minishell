1421d4ec5cc6f1cac1ce6ca1de5db1ed475d31fd
MKM327 <kaanmesum@gmail.com>
2023-02-06 14:29:10 +0300

    t

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*inpt;
+	int		i;
+
+	i = 0;
+	while (i < 10)
+	{
+		inpt = readline("Enter text: ");
+		add_history(inpt);
+		printf("%s", inpt);
+		printf("\n");
+		++i;
+	}
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,8 @@
+typedef struct s_struct
+{
+	char	start[2];
+	char	*command;
+	char	*option;
+	char	*argument;
+	char	end[2];
+}			t_command;
