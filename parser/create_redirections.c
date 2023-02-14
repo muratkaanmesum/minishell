7a3889e88d966fbaaff7e0ee64e78980fd8816de
MKM327 <kaanmesum@gmail.com>
2023-02-14 19:12:09 +0300

    cmt

--- parser/create_redirections.c
+++ parser/create_redirections.c
@@ -6,46 +6,15 @@
 /*   By: mmesum <mmesum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 16:37:54 by mmesum            #+#    #+#             */
-/*   Updated: 2023/02/14 17:17:03 by mmesum           ###   ########.fr       */
+/*   Updated: 2023/02/14 19:12:09 by mmesum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	handle_first(t_token *tokens, int start, int end,
-		t_redirections *redirection)
-{
-	int	last_token;
-
-	if (tokens[0].token == I_REDIRECTION || tokens[0].token == HERE_DOC)
-		redirection->infile = tokens[1].str;
-	else
-	{
-		while (start < end)
-		{
-			if (tokens[start].token == I_REDIRECTION
-				|| tokens[start].token == HERE_DOC)
-			{
-				last_token = start;
-			}
-			start++;
-		}
-		if (tokens[last_token].token != UNKNOWN)
-			redirection->infile = tokens[last_token + 1].str;
-	}
-}
-
-void	handle_redirection(t_redirections *redirection, int start, int end,
-		t_token *tokens)
-{
-	if (start == 0)
-		handle_first(tokens, start, end, redirection);
-	// else
-	// 	handle_rest(tokens, start, end, redirection);
-}
 // cat < test.txt | wc -l | test > outfile.txt
 // < test.txt cat | wc -l | test > outfile.txt
-// cat < test.txt < test2.txt | wc -l | test > outfile.txt
+// cat < test.txt < test2.txt | wc -l < test.txt | test > outfile.txt
 int	does_include(t_token *tokens, int start, int end)
 {
 	while (start < end)
