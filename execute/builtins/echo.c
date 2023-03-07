827e6b42a52ee04c279f6dc9131b174b2d015db7
MKM327 <kaanmesum@gmail.com>
2023-03-07 09:59:59 -0500

    t

--- execute/builtins/echo.c
+++ execute/builtins/echo.c
@@ -1,10 +1,39 @@
 #include "../execute.h"
 
-int	echo(char *arg, char *option)
+int	does_have_option(char **option)
 {
-	if (option == NULL)
-		printf("%s\n", arg);
-	else if (option[0] == 'n')
-		printf("%s", arg);
+	int	i;
+
+	i = 0;
+	while (option[i] != NULL)
+	{
+		if (ft_strncmp("-n", option[i], 2) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+int	print_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i + 1] != NULL)
+		printf("%s ", args[i++]);
+	printf("%s", args[i]);
+	return (1);
+}
+int	echo(char **args, char **option)
+{
+	int	i;
+
+	if (does_have_option(option) == 0)
+	{
+		i = 0;
+		print_args(args);
+		printf("\n");
+	}
+	else
+		print_args(args);
 	return (1);
 }
--- execute/execute.c
+++ execute/execute.c
@@ -7,7 +7,7 @@ int	execute(t_node *head, char **env)
 	if (ft_strncmp(head->command->command, "cd", 2) == 0)
 		cd(head->command->arguments[0], env);
 	if (ft_strncmp(head->command->command, "echo", 4) == 0)
-		echo(head->command->arguments[0], head->command->options[0]);
+		echo(head->command->arguments, head->command->options);
 	if (ft_strncmp(head->command->command, "env", 3) == 0)
 		print_env(env);
 }
