5af8820e6a8344c67a35fb6da3f595cae327a290
MKM327 <kaanmesum@gmail.com>
2023-03-23 11:33:23 -0400

    t

--- main_utils.c
+++ main_utils.c
@@ -6,7 +6,7 @@
 /*   By: mmesum <mmesum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:09:51 by mmesum            #+#    #+#             */
-/*   Updated: 2023/03/23 11:18:06 by mmesum           ###   ########.fr       */
+/*   Updated: 2023/03/23 11:31:11 by mmesum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	first_check_free(t_token *tokens, char *inpt)
 void	exec_rest(t_node *head, t_token *tokens)
 {
 	 expander(head);
-	//execute(head);
+	execute(head);
 	// free(head->execute->input);
 	free_execute(head->execute);
 	free_tree(head);
