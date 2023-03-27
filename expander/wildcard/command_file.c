683b7b7c328cb31a77053161ec6dc5f526894890
Esra <71944354+EsraAblak@users.noreply.github.com>
2023-03-27 13:23:20 +0300

    sonnn

--- expander/wildcard/command_file.c
+++ expander/wildcard/command_file.c
@@ -6,7 +6,7 @@
 /*   By: eablak <eablak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 19:22:00 by eablak            #+#    #+#             */
-/*   Updated: 2023/03/27 13:18:17 by eablak           ###   ########.fr       */
+/*   Updated: 2023/03/27 13:22:35 by eablak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,32 +98,30 @@ int	count_wildcard(char *prefix, char *suffix, int *count)
 void	expand_wildcard(char *prefix, char *suffix, char **return_files,
 		int *index)
 {
-	char	*data;
-	char	buf[1024];
-	char	*path;
-	char	**files;
-	int		i;
+	t_wild	*wild;
 
+	wild = malloc(sizeof(t_wild) * 1);
 	if (suffix == NULL)
 	{
 		return_files[*index] = ft_strdup(prefix);
 		*index += 1;
 		return ;
 	}
-	data = find_data(suffix);
-	path = getcwd(buf, 1024);
-	path = add_slash(path);
-	path = new_path(path, prefix);
-	files = mutual_wildcard(data, path);
+	wild->data = find_data(suffix);
+	wild->path = getcwd(wild->buf, 1024);
+	wild->path = add_slash(wild->path);
+	wild->path = new_path(wild->path, prefix);
+	wild->files = mutual_wildcard(wild->data, wild->path);
 	suffix = cut_suffix(suffix);
-	i = 0;
-	while (files[i])
+	wild->i = 0;
+	while (wild->files[wild->i])
 	{
-		files[i] = prefix_add_file(prefix, files[i]);
-		expand_wildcard(files[i], suffix, return_files, index);
-		i++;
+		wild->files[wild->i] = prefix_add_file(prefix, wild->files[wild->i]);
+		expand_wildcard(wild->files[wild->i], suffix, return_files, index);
+		wild->i++;
 	}
-	free_double_ptr(files);
-	free(data);
+	free_double_ptr(wild->files);
+	free(wild->data);
+	free(wild);
 	return ;
 }
